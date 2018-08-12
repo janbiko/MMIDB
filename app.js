c185aaa3eb4779d320cc3ba912f8dd61fdc4fd0d
Angis <Dominik.klis@gmx.de>
2018-08-12 10:20:18 +0200

    task-overview buttons working
    man kann jetzt aufträge annehmen und abschließen, beim Abschließen wird die "CurrentPosition" des Employees auf die "DestinationAddress" des abgeschlossenen Tasks gesetzt.

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 "use strict";
 
 const express = require('express'),
-	  http = require('http'),
-	  mysql = require('mysql'),
-	  bodyParser = require('body-parser'),
-    dateFormat = require('dateformat');
+  http = require('http'),
+  mysql = require('mysql'),
+  bodyParser = require('body-parser'),
+  dateFormat = require('dateformat');
 
 const app = express();
 
 let now = new Date();
 
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
@@ -31,8 +31,8 @@ const baseURL = "http://localhost:3000/";
 
 
 // getting index page
-app.get('/', function(req, res) {
-  
+app.get('/', function (req, res) {
+
   res.render('pages/index', {
     siteTitle: siteTitle,
     pageTitle: "Kuriermanagement"
@@ -40,7 +40,7 @@ app.get('/', function(req, res) {
 });
 
 // getting manager interface
-app.get('/manager', function(req, res) {
+app.get('/manager', function (req, res) {
 
   res.render('pages/manager', {
     siteTitle: siteTitle,
@@ -49,9 +49,9 @@ app.get('/manager', function(req, res) {
 });
 
 // getting employees overview
-app.get('/manager/employees', function(req, res) {
+app.get('/manager/employees', function (req, res) {
 
-  con.query("SELECT * FROM Employees", function(err, result) {
+  con.query("SELECT * FROM Employees", function (err, result) {
     res.render('pages/employees', {
       siteTitle: siteTitle,
       pageTitle: "Kuriermanagement",
@@ -73,22 +73,22 @@ app.get('/manager/employees/add', function(req, res) {
 */
 
 // inserting values from form into database
-app.post('/manager/employees/add', function(req, res) {
-  
+app.post('/manager/employees/add', function (req, res) {
+
   let query = "INSERT INTO Employees (Name, CurrentPosition) VALUES ( '" +
-              req.body.employeeName + "', '" +
-              req.body.employeePosition + "')";
-  
-  con.query(query, function(err, result) {
+    req.body.employeeName + "', '" +
+    req.body.employeePosition + "')";
+
+  con.query(query, function (err, result) {
     res.redirect(baseURL + "manager/employees");
   });
 });
 
 
 // getting edit employee page
-app.get('/manager/employees/edit/:employee_id', function(req, res) {
-  
-  con.query("SELECT * FROM Employees WHERE EmployeeID = '" + req.params.employee_id + "'", function(err, result) {
+app.get('/manager/employees/edit/:employee_id', function (req, res) {
+
+  con.query("SELECT * FROM Employees WHERE EmployeeID = '" + req.params.employee_id + "'", function (err, result) {
     res.render('pages/edit-employee', {
       siteTitle: siteTitle,
       pageTitle: "Editing employee: " + result[0].Name,
@@ -99,70 +99,70 @@ app.get('/manager/employees/edit/:employee_id', function(req, res) {
 
 
 // updating employee data
-app.post('/manager/employees/edit/:employee_id', function(req, res) {
-  
+app.post('/manager/employees/edit/:employee_id', function (req, res) {
+
   let query = "UPDATE Employees SET " +
-              "Name = '" + req.body.employeeName + "', " +
-              "CurrentPosition = '" + req.body.employeePosition + "' " +
-              "WHERE Employees.EmployeeID = " + req.body.employeeId;
-  
-  con.query(query, function(err, result) {
-    if(result.affectedRows) res.redirect(baseURL + "manager/employees");
+    "Name = '" + req.body.employeeName + "', " +
+    "CurrentPosition = '" + req.body.employeePosition + "' " +
+    "WHERE Employees.EmployeeID = " + req.body.employeeId;
+
+  con.query(query, function (err, result) {
+    if (result.affectedRows) res.redirect(baseURL + "manager/employees");
   });
 });
 
 
 // delete employee from database
-app.get('/manager/employees/delete/:employee_id', function(req, res) {
-  
+app.get('/manager/employees/delete/:employee_id', function (req, res) {
+
   let query = "DELETE FROM Employees WHERE EmployeeID = " + req.params.employee_id;
-  
-  con.query(query, function(err, result) {
-    if(result.affectedRows) res.redirect(baseURL + "manager/employees");
+
+  con.query(query, function (err, result) {
+    if (result.affectedRows) res.redirect(baseURL + "manager/employees");
   });
-  
+
 });
 
-app.get('/manager/tasks', function(req, res) {
+app.get('/manager/tasks', function (req, res) {
 
-  con.query("SELECT t.TaskID, t.Status, t.DeliveryTime, t.PickupAddress, t.DestinationAddress, e.Name " + 
-  "FROM Tasks t LEFT JOIN Employees e ON e.EmployeeID = t.EmployeeID", function(err, result) {
-    res.render('pages/tasks', {
-      siteTitle: siteTitle,
-      pageTitle: "Kuriermanagement",
-      items: result
+  con.query("SELECT t.TaskID, t.Status, t.DeliveryTime, t.PickupAddress, t.DestinationAddress, e.Name " +
+    "FROM Tasks t LEFT JOIN Employees e ON e.EmployeeID = t.EmployeeID", function (err, result) {
+      res.render('pages/tasks', {
+        siteTitle: siteTitle,
+        pageTitle: "Kuriermanagement",
+        items: result
+      });
     });
-  });
 });
 
 // add task to database
-app.post('/manager/tasks/add', function(req, res) {
+app.post('/manager/tasks/add', function (req, res) {
 
   let query = "INSERT INTO Tasks (Status, DeliveryTime, PickupAddress, DestinationAddress) VALUES ( '" +
-              "Open', '" + 
-              req.body.deliveryTime + "', '" + 
-              req.body.pickupAddress + "', '" + 
-              req.body.destinationAddress + "')";
+    "Open', '" +
+    req.body.deliveryTime + "', '" +
+    req.body.pickupAddress + "', '" +
+    req.body.destinationAddress + "')";
 
-  con.query(query, function(err, result) {
+  con.query(query, function (err, result) {
     res.redirect(baseURL + "manager/tasks");
   });
 });
 
 // delete task from database
-app.get('/manager/tasks/delete/:task_id', function(req, res) {
+app.get('/manager/tasks/delete/:task_id', function (req, res) {
 
   let query = "DELETE FROM Tasks WHERE TaskID = " + req.params.task_id;
 
-  con.query(query, function(err, result) {
-    if(result.affectedRows) res.redirect(baseURL + "manager/tasks");
+  con.query(query, function (err, result) {
+    if (result.affectedRows) res.redirect(baseURL + "manager/tasks");
   });
 });
 
-app.get('/employee', function(req, res) {
+app.get('/employee', function (req, res) {
 
   let query = "SELECT EmployeeID, Name FROM Employees";
-  con.query(query, function(err, result) {
+  con.query(query, function (err, result) {
     res.render('pages/employee', {
       siteTitle: siteTitle,
       pageTitle: "Kuriermanagement",
@@ -171,19 +171,66 @@ app.get('/employee', function(req, res) {
   });
 });
 
-app.get('/employee/:employee_id/tasks', function(req, res) {
+app.get('/employee/:employee_id/tasks', function (req, res) {
 
-  con.query("SELECT t.TaskID, t.Status, t.DeliveryTime, t.PickupAddress, t.DestinationAddress, e.Name, t.EmployeeID " + 
-  "FROM Tasks t LEFT JOIN Employees e ON e.EmployeeID = t.EmployeeID", function(err, result) {
-    res.render('pages/task-overview', {
-      siteTitle: siteTitle,
-      pageTitle: "Kuriermanagement",
-      items: result,
+  con.query("SELECT t.TaskID, t.Status, t.DeliveryTime, t.PickupAddress, t.DestinationAddress, e.Name, t.EmployeeID " +
+    "FROM Tasks t LEFT JOIN Employees e ON e.EmployeeID = t.EmployeeID", function (err, result) {
+      res.render('pages/task-overview', {
+        siteTitle: siteTitle,
+        pageTitle: "Kuriermanagement",
+        items: result,
+        id: req.params.employee_id,
+      });
+    });
+});
+
+app.post('/employee/:employee_id/tasks/accept/:task_id', function (req, res) {
+
+  let query = "UPDATE Tasks SET Status = 'Assigned', EmployeeID = '" + req.params.employee_id + "' WHERE TaskID = '" + req.params.task_id + "'";
+
+  con.query(query, function (err, result) {
+    if (result.affectedRows) res.redirect(baseURL + "employee/" + req.params.employee_id + "/tasks");
+  });
+});
+
+app.post('/employee/:employee_id/tasks/done/:task_id/:destinationAddress', function (req, res) {
+  let query1 = "UPDATE Tasks SET Status = 'Done', EmployeeID = '" + req.params.employee_id + "' WHERE TaskID = '" + req.params.task_id + "';"
+  let query2 = "UPDATE Employees SET CurrentPosition = '" + req.params.destinationAddress +"' WHERE EmployeeID = '" + req.params.employee_id + "';"
+  
+  
+  con.beginTransaction(function (err) {
+    if (err) { throw err; }
+    con.query(query1, function (err, result) {
+      if (err) {
+        con.rollback(function () {
+          throw err;
+        });
+      }
+
+      var log = result.insertId;
+
+      con.query(query2, log, function (err, result) {
+        if (err) {
+          con.rollback(function () {
+            throw err;
+          });
+        }
+        con.commit(function (err) {
+          if (err) {
+            con.rollback(function () {
+              throw err;
+            });
+          }
+          console.log('Transaction Complete.');
+          if (result) res.redirect(baseURL + "employee/" + req.params.employee_id + "/tasks");
+          con.commit();
+        });
+      });
     });
   });
 });
 
 // starting server
-let server = app.listen(3000, function() {
+let server = app.listen(3000, function () {
   console.log("Server started on port 3000...");
 });
